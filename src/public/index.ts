b6098b38a6c4a6cdb0c22b8887ef58acab730928
Fidel Lascano <fidel.lascano@conauto.com.ec>
2021-09-07 14:39:29 -0500

    Config Oracle connection

--- src/public/index.ts
+++ src/public/index.ts
@@ -4,8 +4,20 @@ import {fnCreateFile, fnFileExists} from "./files/Files";
 import {fnMainMenu} from "./menu/Menu";
 import ColorText from "./Util/ColorText";
 import {commands} from "./Util/Vars";
+import oracledb from "oracledb";
+import path from "path";
 
+oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
+const initConnection = {libDir: path.resolve(process.cwd(), "oracle/instantclient_win")}
 
+const dbConnection = {
+    user: "username"
+    ,password: "secretpassword"
+    ,connectString: `(DESCRIPTION = (ADDRESS_LIST = (ADDRESS = (COMMUNITY = tcp.world)(PROTOCOL = TCP)(Host = 192.168.100.100)(Port = 1521)))(CONNECT_DATA = (SID = dbname)))`
+}
+
+oracledb.initOracleClient(initConnection);
+oracledb.getConnection(dbConnection).then(value => console.log('Estas Conectado...'));
 
 const fnRunCli = ():void => {
     const  args = minimist(process.argv.slice(2));
@@ -20,4 +32,4 @@ const fnRunCli = ():void => {
     else {colorText.error(`${args["_"][0]} was not a command`);}
 }
 
-fnRunCli();
+// fnRunCli();
